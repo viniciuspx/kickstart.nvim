32a6036aa512e7ba7da9a8961f475da1029f0177
Vinicius Gabriel Santos <vinicius.gabriel.px@gmail.com>
2024-04-28 10:34:30 -0300

    update with new plugings and configs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -13,4 +13,34 @@ return {
   config = function()
     require('nvim-tree').setup {}
   end,
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+  {
+    'folke/todo-comments.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+  -- TODO: Nice todo!
+  -- HACK: Some macarronic code
+  -- PERF: i cant beliave i done this
+  -- NOTE: noted
+  -- FIX: fix this
+  -- WARNING: This is a warning
+  'NvChad/nvim-colorizer.lua',
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        -- config
+      }
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  },
+
+  { 'akinsho/git-conflict.nvim', version = '*', config = true },
+  'f-person/git-blame.nvim',
+  { 'bluz71/vim-moonfly-colors', name = 'moonfly', lazy = false, priority = 1000 },
 }
